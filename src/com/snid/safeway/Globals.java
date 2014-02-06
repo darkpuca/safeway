fcbd2cbb3a87607f92ac9d68f461e640140652c4
Kim Dongkyu <darkpuca@gmail.com>
2014-02-07 01:19:52 +0900

    2014-2-7 * GCM client sample 구현

--- src/com/snid/safeway/Globals.java
+++ src/com/snid/safeway/Globals.java
@@ -4,6 +4,12 @@ public class Globals
 {
 	public static final String[] sample_strings = { "string1", "string2", "string3", "string4", "string5", "string6", "string7", "string8", "string9" };
 
-	static String SERVER_URI = "http://safeway.snid.kr/";
+	public static final String SERVER_URI = "http://safeway.snid.kr/";
+	public static final String GCM_SENDER_ID = "1048918795666";
 	
+	public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    public static final String PROPERTY_APP_VERSION = "appVersion";
+	public static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
 }
